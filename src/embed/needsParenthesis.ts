c45bf426fe2219cddcae15fe4851d9280e7d02f4
Angus Morton <angus.m.morton@gmail.com>
2024-02-21 21:29:36 +1100

    Remove parenthesis

--- src/embed/needsParenthesis.ts
+++ src/embed/needsParenthesis.ts
@@ -3,10 +3,15 @@ import {
   endsWithBang,
   endsWithBrace,
   endsWithBracket,
+  endsWithParenthesis,
   isStringDoc,
 } from "./util";
 
 export function needsParenthesis(doc: Doc): boolean {
+  if (endsWithParenthesis(doc)) {
+    return false;
+  }
+
   if (endsWithBrace(doc)) {
     return false;
   }
--- src/embed/util.ts
+++ src/embed/util.ts
@@ -239,6 +239,40 @@ export function endsWithBang(doc: Doc): boolean {
   return false;
 }
 
+export function endsWithParenthesis(doc: Doc): boolean {
+  if (typeof doc === "string") {
+    return doc[doc.length - 1] === ")";
+  }
+
+  if (!Array.isArray(doc)) {
+    switch (doc.type) {
+      case "align":
+      case "break-parent":
+      case "cursor":
+      case "fill":
+      case "if-break":
+      case "indent":
+      case "indent-if-break":
+      case "label":
+      case "line":
+      case "line-suffix":
+      case "line-suffix-boundary":
+      case "trim":
+        return false;
+      case "group":
+        return endsWithParenthesis(doc.contents);
+    }
+  }
+
+  for (let i = doc.length - 1; i >= 0; i--) {
+    if (endsWithParenthesis(doc[i])) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 export function isStringDoc(doc: Doc): boolean {
   if (typeof doc === "string") {
     return true;
