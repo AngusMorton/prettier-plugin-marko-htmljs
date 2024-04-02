38beee6ead67c04e2b5f901cc4f4aaff525b441a
AngusMorton <angus.m.morton@gmail.com>
2024-04-02 22:04:48 +1100

    Handle class embed errors

--- src/embed/internal/embedClass.ts
+++ src/embed/internal/embedClass.ts
@@ -10,20 +10,29 @@ export function embedClass(
     "class __PLACEHOLDER__",
   );
   return async (textToDoc) => {
-    const body = await textToDoc(mutatedClassBody, {
-      parser: "babel-ts",
-    });
+    try {
+      const body = await textToDoc(mutatedClassBody, {
+        parser: "babel-ts",
+      });
 
-    // The first doc in the array is the class name with the placeholder.
-    // It will look something like "class __PLACEHOLDER__ {" which we need
-    // to transform back to "class {".
-    if (Array.isArray(body)) {
-      const classNameDoc = body[0];
-      if (typeof classNameDoc === "string") {
-        body[0] = classNameDoc.replace(/__PLACEHOLDER__\s/, "");
+      // The first doc in the array is the class name with the placeholder.
+      // It will look something like "class __PLACEHOLDER__ {" which we need
+      // to transform back to "class {".
+      if (Array.isArray(body)) {
+        const classNameDoc = body[0];
+        if (typeof classNameDoc === "string") {
+          body[0] = classNameDoc.replace(/__PLACEHOLDER__\s/, "");
+        }
       }
-    }
 
-    return body;
+      return body;
+    } catch (error) {
+      if (process.env.PRETTIER_DEBUG) {
+        throw error;
+      }
+
+      console.error(error);
+      return [node.valueLiteral];
+    }
   };
 }
